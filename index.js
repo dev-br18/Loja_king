56b45efd26b7efe7cb84c1c06c981dc590bd1d21
Deivid Daniel <79475211+dev-br18@users.noreply.github.com>
2023-09-24 19:35:16 -0300

    adicionando modal e carrosell dos produtos

---
+++ index.js
@@ -0,0 +1,63 @@
+const carousel = document.querySelector('.carousel');
+let isDragging = false;
+let startX, scrollLeft;
+
+
+const openModalButtons = document.querySelectorAll('.open-modal-button');
+const modal = document.getElementById('myModal');
+const modalTitle = document.querySelector('.modal-title');
+const modalDescription = document.querySelector('.modal-description');
+const modalImage = document.querySelector('.modal-image');
+const modalPrice = document.querySelector('.modal-price')
+const closeModalButton = document.querySelector('.close');
+
+// Função para fechar o modal
+function closeModal() {
+    modal.style.display = 'none'; // Oculta o modal
+  }
+
+closeModalButton.addEventListener('click', closeModal);
+
+// Adicione um ouvinte de evento de clique a todos os botões "Ver Detalhes"
+openModalButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+    const title = button.getAttribute('data-title');
+    const description = button.getAttribute('data-description');
+    const imageSrc = button.getAttribute('data-image');
+    const price = button.getAttribute('data-price');
+
+    modalTitle.textContent = title;
+    modalDescription.textContent = description;
+    modalPrice.textContent = price;
+    modalImage.src = imageSrc;
+
+    modal.style.display = 'block'; // Exibe o modal com os detalhes específicos do celular
+   });
+});
+
+  
+
+carousel.addEventListener('mousedown', (e) => {
+  isDragging = true;
+  startX = e.pageX - carousel.offsetLeft;
+  scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener('mouseup', () => {
+  isDragging = false;
+});
+
+carousel.addEventListener('mouseleave', () => {
+  isDragging = false;
+});
+
+carousel.addEventListener('mousemove', (e) => {
+  if (!isDragging) return;
+  e.preventDefault();
+  const x = e.pageX - carousel.offsetLeft;
+  const walk = (x - startX) * 1.5;
+  carousel.scrollLeft = scrollLeft - walk;
+});
+
+
+
